c068efda394eb81b069b8e539937290ade7aa0cd
TANN <tanpawin@hotmail.com>
2026-07-29 17:49:25 +0700

    add new tech skill

--- src/data.ts
+++ src/data.ts
@@ -1,46 +1,63 @@
 // src/data.ts
 export const PORTFOLIO_DATA = {
-    name: "ภวินท์ ฐิติพงศ์",
-    role: "Web Application Developer",
-    profileImage: "/profile.jpg",
-    bio: "สวัสดีครับ แทนครับ ผมกำลังศึกษาอยู่สาขาวิทยาการคอมพิวเตอร์ปี4 \n มีความสนใจในด้าน UX/UI Designer และ Frontend Developer ครับ",
-    contact: {
-        email: "pawin.thitipong@gmail.com",
-        github: "https://github.com/tanpawin",
-        linkedin: "https://www.linkedin.com/in/pawin-thitipong-7ab3843b2",
-        phone: "093-938-7831",
+  name: "ภวินท์ ฐิติพงศ์",
+  role: "Web Application Developer",
+  profileImage: "/profile.jpg",
+  bio: "สวัสดีครับ แทนครับ ผมกำลังศึกษาอยู่สาขาวิทยาการคอมพิวเตอร์ปี4 \n มีความสนใจในด้าน UX/UI Designer และ Frontend Developer ครับ",
+  contact: {
+    email: "pawin.thitipong@gmail.com",
+    github: "https://github.com/tanpawin",
+    linkedin: "https://www.linkedin.com/in/pawin-thitipong-7ab3843b2",
+    phone: "093-938-7831",
+  },
+  education: [
+    {
+      school: "มหาวิทยาลัยราชภัฏพระนคร",
+      abbr: "PNRU",
+      major: "คณะวิทยาศาสตร์และเทคโนโลยี",
+      degree: "สาขาวิทยาการคอมพิวเตอร์",
+      period: "2023 - ปัจจุบัน",
+      gpa: "3.52",
     },
-    education: [
-        {
-            school: "มหาวิทยาลัยราชภัฏพระนคร",
-            abbr: "PNRU",
-            major: "คณะวิทยาศาสตร์และเทคโนโลยี",
-            degree: "สาขาวิทยาการคอมพิวเตอร์",
-            period: "2023 - ปัจจุบัน",
-            gpa: "3.52",
-        },
-    ],
-    skillCategories: [
-        {
-            name: "Frontend",
-            skills: ["HTML", "CSS", "Tailwind CSS", "JavaScript", "TypeScript", "React", "Next.js"],
-        },
-        {
-            name: "Backend & ORM",
-            skills: ["Supabase", "Prisma"],
-        },
-        {
-            name: "Database",
-            skills: ["MySQL","PostgreSQL"],
-        },
-        {
-            name: "Tools & Design",
-            skills: ["Clerk", "Figma", "GitHub", "VS Code", "XAMPP", "Vercel"],
-        },
-    ],
+  ],
+  skillCategories: [
+    {
+      name: "Frontend",
+      skills: [
+        "HTML",
+        "CSS",
+        "Tailwind CSS",
+        "JavaScript",
+        "TypeScript",
+        "React",
+        "Next.js",
+      ],
+    },
+    {
+      name: "Backend & ORM",
+      skills: ["Node.js", "Supabase", "Prisma", "PHP"],
+    },
+    {
+      name: "Database",
+      skills: ["MySQL", "PostgreSQL"],
+    },
+    {
+      name: "Tools & Design",
+      skills: [
+        "Clerk",
+        "Figma",
+        "GitHub",
+        "VS Code",
+        "XAMPP",
+        "Vercel",
+        "Mermaid",
+        "Render",
+      ],
+    },
+  ],
 
-    // เพิ่มผลงานที่นี่
-    /* featuredProjects: [
+  // เพิ่มผลงานที่นี่
+  /* featuredProjects: [
         {
             id: "manual-1",
             name: "Full-Stack Web Application (📌Dummy Test ไม่ใช่โปรเจกต์จริง)",
@@ -50,4 +67,4 @@ export const PORTFOLIO_DATA = {
             homepage: "", // ลิงก์เว็บ (ถ้ามี)
         },
     ] */
-}
+};
